56487380ab38455578765554a76fe1aea83b7c15
zhibilin <83932204+zhibilin@users.noreply.github.com>
2025-08-26 10:21:56 +0000

    Deploying to gh-pages from @ zhibilin/zhibilin.github.io@9b4e1fe44491aa4fb8bf0d90aa072619f39caea0 🚀

---
+++ assets/README455.html-DMzvHWjM.js
@@ -0,0 +1,15 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as o,o as n}from"./app-ES30thiV.js";import"./vendor-B0wGJQsQ.js";const r={};function a(m,i){return n(),t("div",null,[o(` ---
+home: true
+portfolio: true
+title: NODE1
+icon: home
+welcome: Why Choose Node1?
+name: BLOCKCHAIN| NODE | STAKING
+avatar: https://vuepress.vuejs.org/images/hero.png
+titles:
+  - 👋 Services on the blockchain
+  - 🚀 Global Infrastructure
+  - ✨ High TPS Capacity
+links:
+footer: false
+--- `)])}const l=e(r,[["render",a]]),h=JSON.parse('{"path":"/README455.html","title":"","lang":"EN-US","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-08-26T10:19:56.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"NODE1 bLOG\\",\\"url\\":\\"node1.me\\"}]}"],["meta",{"property":"og:url","content":"https://node1.me/README455.html"}],["meta",{"property":"og:site_name","content":"NODE1"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"EN-US"}],["meta",{"property":"og:updated_time","content":"2025-08-26T10:19:56.000Z"}],["meta",{"property":"article:modified_time","content":"2025-08-26T10:19:56.000Z"}]]},"git":{"createdTime":1727247390000,"updatedTime":1756203596000,"contributors":[{"name":"muyi","username":"muyi","email":"1986094870@qq.com","commits":4,"url":"https://github.com/muyi"},{"name":"ydn","username":"ydn","email":"1986094870@qq.com","commits":4,"url":"https://github.com/ydn"}]},"readingTime":{"minutes":0.11,"words":32},"filePathRelative":"README455.md","excerpt":"<!-- ---\\nhome: true\\nportfolio: true\\ntitle: NODE1\\nicon: home\\nwelcome: Why Choose Node1?\\nname: BLOCKCHAIN| NODE | STAKING\\navatar: https://vuepress.vuejs.org/images/hero.png\\ntitles:\\n  - 👋 Services on the blockchain\\n  - 🚀 Global Infrastructure\\n  - ✨ High TPS Capacity\\nlinks:\\nfooter: false\\n--- -->\\n"}');export{l as comp,h as data};
